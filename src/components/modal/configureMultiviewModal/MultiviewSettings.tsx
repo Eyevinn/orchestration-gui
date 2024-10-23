b445c9e9916b6bfd5db9e24695b0134d892eded8
malmen237 <linda.malm@eyevinn.se>
2024-10-23 07:05:54 +0200

    fix: removed unused import

--- src/components/modal/configureMultiviewModal/MultiviewSettings.tsx
+++ src/components/modal/configureMultiviewModal/MultiviewSettings.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslate } from '../../../i18n/useTranslate';
 import { MultiviewSettings } from '../../../interfaces/multiview';
 import { TMultiviewLayout } from '../../../interfaces/preset';
