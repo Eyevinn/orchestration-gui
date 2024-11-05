d0a968e6aabda00529b6c16cbc5bf5b702793bf7
Saelmala <sandra.larsson@eyevinn.se>
2024-11-05 17:02:43 +0100

    fix: sources not being removed from pipeline

--- src/app/production/[id]/page.tsx
+++ src/app/production/[id]/page.tsx
@@ -801,7 +801,7 @@ export default function ProductionConfiguration({ params }: PageProps) {
     }
   };
 
-  const handleRemoveSource = async () => {
+  const handleRemoveSource = async (ingestSource?: SourceWithId) => {
     if (productionSetup && productionSetup.isActive && selectedSourceRef) {
       const multiviews =
         productionSetup.production_settings.pipelines[0].multiviews;
@@ -945,7 +945,19 @@ export default function ProductionConfiguration({ params }: PageProps) {
         }
       }
 
-      const updatedSetup = removeSetupItem(selectedSourceRef, productionSetup);
+      const ingestSourceId =
+        ingestSource !== undefined
+          ? await getIngestSourceId(
+              ingestSource.ingest_name,
+              ingestSource.ingest_source_name
+            )
+          : undefined;
+
+      const updatedSetup = removeSetupItem(
+        selectedSourceRef,
+        productionSetup,
+        ingestSourceId
+      );
 
       if (!updatedSetup) return;
       updateSourceInputSlotOnMultiviewLayouts(updatedSetup).then((result) => {
@@ -954,6 +966,7 @@ export default function ProductionConfiguration({ params }: PageProps) {
         updateMultiview(selectedSourceRef, result);
         setRemoveSourceModal(false);
         setSelectedSourceRef(undefined);
+        setSelectedSource(undefined);
       });
     }
   };
@@ -1096,6 +1109,8 @@ export default function ProductionConfiguration({ params }: PageProps) {
                     ingestSource?: ISource
                   ) => {
                     if (productionSetup && productionSetup.isActive) {
+                      // MBY THIS WORKS???????
+                      setSelectedSource(ingestSource);
                       setSelectedSourceRef(source);
                       setRemoveSourceModal(true);
                     } else if (productionSetup) {
@@ -1128,12 +1143,13 @@ export default function ProductionConfiguration({ params }: PageProps) {
                   }}
                   loading={loading}
                 />
+                {/* Hur få in ISource här? */}
                 {removeSourceModal && selectedSourceRef && (
                   <RemoveSourceModal
                     name={selectedSourceRef.label}
                     open={removeSourceModal}
                     onAbort={handleAbortRemoveSource}
-                    onConfirm={handleRemoveSource}
+                    onConfirm={() => handleRemoveSource(selectedSource)}
                     status={deleteSourceStatus}
                     loading={
                       loadingDeleteStream ||
