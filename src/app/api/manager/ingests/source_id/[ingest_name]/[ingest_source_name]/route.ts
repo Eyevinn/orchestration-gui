deb5a0f5d501d39f45c2c2cd80ea1e9af0f3e749
Saelmala <sandra.larsson@eyevinn.se>
2024-11-07 15:47:23 +0100

    fix: prettier test fail

--- src/app/api/manager/ingests/source_id/[ingest_name]/[ingest_source_name]/route.ts
+++ src/app/api/manager/ingests/source_id/[ingest_name]/[ingest_source_name]/route.ts
@@ -23,7 +23,11 @@ export async function GET(
   try {
     const ingestUuid = await getUuidFromIngestName(params.ingest_name, false);
     const sourceId = ingestUuid
-      ? await getSourceIdFromSourceName(ingestUuid, params.ingest_source_name, false)
+      ? await getSourceIdFromSourceName(
+          ingestUuid,
+          params.ingest_source_name,
+          false
+        )
       : 0;
     return new NextResponse(JSON.stringify(sourceId), { status: 200 });
   } catch (error) {
